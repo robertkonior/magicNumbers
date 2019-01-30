7163da5fe6ade4bba691f4fbab7b2e3a325fc76a
robertkonior <praca.robert.konior@gmail.com>
2019-01-30 10:54:30 +0100

    initial commit

---
+++ src/app/AppRunner.java
@@ -0,0 +1,13 @@
+package app;
+
+import java.util.Scanner;
+
+public class AppRunner {
+
+
+    public static void main(String[] args) {
+        System.out.println("enter file path to validate");
+        Scanner scanner = new Scanner(System.in);
+        String filePath = scanner.nextLine();
+    }
+}
